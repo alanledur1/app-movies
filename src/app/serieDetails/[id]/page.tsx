ab7cd0fd4cd0e36944a89642ad2b2a8a2f1fa4dd
alanledur1 <ledur77conta2@gmail.com>
2024-10-10 20:48:25 -0300

    feat: New component for the exhibition of seasons and episodes

--- src/app/serieDetails/[id]/page.tsx
+++ src/app/serieDetails/[id]/page.tsx
@@ -8,8 +8,7 @@ import useSerieDetails from '@/hooks/useSerieDetails';
 
 export default function SerieDetails() {
     const { id } = useParams(); // Use useParams para obter o parâmetro da URL
-    const { serie, seasons, trailer, loading, error } = useSerieDetails(id);
-
+    const { serie, trailer, loading, error } = useSerieDetails(id);
 
     if (loading) return <div>Carregando...</div>;
     if (error) return <div>Erro: {error.message}</div>;
@@ -20,10 +19,10 @@ export default function SerieDetails() {
                 <div className='content'> 
                     <img
                         src={`https://image.tmdb.org/t/p/original${serie.poster_path}`}
-                        alt={serie.name}
+                        alt={serie.title}
                     />
                     <div className='details-content'>
-                        <h1>{serie.name}</h1>
+                        <h1>{serie.title}</h1>
                         <p className='description-serie'>{serie.overview}</p>
                         <p><strong>Data de lançamento:</strong> {serie.release_date}</p>
                         <p><strong>Duração:</strong> {serie.runtime} minutos</p>
@@ -33,46 +32,18 @@ export default function SerieDetails() {
                     </div>
                 </div>
             )}
-
-            <div className="container">
-                <div className="bottom-content">
-                {seasons && seasons.length > 0 && (
-                    <div className="seasons-list">
-                        <h2>Temporadas</h2>
-                        <ul>
-                            {seasons.map(season => (
-                                <li key={season.season_number}>
-                                    <strong>Temporada {season.season_number}:</strong> {season.episode_count} episódios
-                                    <p><strong>Data de lançamento:</strong> {season.air_date}</p>
-                                    <p>{season.overview}</p>
-                                    {season.poster_path && (
-                                        <img
-                                            src={`https://image.tmdb.org/t/p/w200${season.poster_path}`}
-                                            alt={season.name}
-                                            className="season-poster"
-                                        />
-                                    )}
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                )}
-                </div>
+            {trailer && (
                 <div className="trailer-container">
-                    {trailer && (
-                        <div className="trailer-content">
-                            <iframe
-                                src={trailer}
-                                title="YouTube video player"
-                                frameBorder="0"
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                allowFullScreen
-                                className='iframe'
-                            ></iframe>
-                        </div>
-                    )}
+                    <iframe
+                        src={trailer}
+                        title="YouTube video player"
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                        className='iframe'
+                    ></iframe>
                 </div>
-            </div>
+            )}
         </div>
     );
 }
